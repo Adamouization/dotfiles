88435c931c643ed1df25787d7a8a8751714f2c6e
Adam Jaamour <aj645@bath.ac.uk>
2017-10-26 14:09:44 +0100

    Create activate_virtual_env.sh

---
+++ bash_scripts/activate_virtual_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# bash file to activate virtual environment
+# replace <your_environment> with the directory in which your environment lives in
+
+source ~/Environments/<your_environment>/bin/activate
+echo "virtual env successfully activated"
